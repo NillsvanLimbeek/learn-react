ccab5087118f38c19a88963294fa84b55af7e8dc
Nills <nills.van.limbeek@live.nl>
2020-01-27 06:40:39 +0100

    navbar modal start

--- kanban/src/components/boards-button/BoardsButton.js
+++ kanban/src/components/boards-button/BoardsButton.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './BoardsButton.scss';
 
 export const BoardsButton = () => {
+    const [modal, setModal] = useState(false);
+
     return (
-        <div className="boards-button">
+        <div className="boards-button" onClick={() => setModal(true)}>
             <i className="boards-button__icon fas fa-th" />
             <p className="boards-button__title">Boards</p>
+
+            {modal && <p>Modal Open</p>}
         </div>
     );
 };
